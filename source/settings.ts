38ab77fd0edcfae44de6192b22857fcb34472cb8
chkt <ck@chkt.one>
2020-02-06 12:00:03 +0100

    Add tslint annotations for inherited interface

--- source/settings.ts
+++ source/settings.ts
@@ -9,11 +9,13 @@ interface SequencerSettings extends Required<SequencerConfig> {}
 
 
 function createDefaultSettings() : SequencerSettings {
+	// tslint:disable-next-line:object-literal-sort-keys
 	return { next : 0, maxBlocked : Number.POSITIVE_INFINITY };
 }
 
 function normalizeSettings(settings:SequencerSettings) : SequencerSettings {
 	return {
+		// tslint:disable-next-line:object-literal-sort-keys
 		next : Math.max(settings.next % Number.MAX_SAFE_INTEGER, 0),
 		maxBlocked : Math.max(settings.maxBlocked, 0)
 	};
